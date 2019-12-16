7ed5d7f6f0130721196cf4f9aa507afb45db2294
gumtrip <gumtrip@126.com>
2019-12-16 18:16:02 +0800

    加入右下角按钮

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <mt-palette-button content="+" direction="lt" mainButtonStyle="color:#fff;background-color:#26a2ff;" style="left:30px;">
+    <mt-palette-button id="contact-palette" content="+" direction="lt" mainButtonStyle="color:#fff;background-color:#26a2ff;">
       <div class="my-icon-button">1</div>
       <div class="my-icon-button">2</div>
       <div class="my-icon-button">3</div>
@@ -23,5 +23,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+  #contact-palette{position: fixed;bottom: 10%;right: 10%}
 </style>
