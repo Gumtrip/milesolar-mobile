333a27a550d88660caf655be333b8a7b5e10c52a
gumtrip <gumtrip@126.com>
2019-12-16 18:06:11 +0800

    加入右下角按钮

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <div />
+  <div>
+    <mt-palette-button content="+" direction="lt" mainButtonStyle="color:#fff;background-color:#26a2ff;" style="left:30px;">
+      <div class="my-icon-button">1</div>
+      <div class="my-icon-button">2</div>
+      <div class="my-icon-button">3</div>
+    </mt-palette-button>
+  </div>
 </template>
 
 <script>
