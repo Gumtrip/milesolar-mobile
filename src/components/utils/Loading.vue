f1255430029a25679a50490cf1e9a1f159a76f2f
gumtrip <gumtrip@126.com>
2019-12-30 10:31:26 +0800

    没有更多产品变更为toast 样式

--- src/components/utils/Loading.vue
+++ src/components/utils/Loading.vue
@@ -3,13 +3,12 @@
     <div class="text_center">
       <mt-spinner v-show="loading" type="snake" class="snake" color="#26a2ff"></mt-spinner>
     </div>
-    <div v-if="!moreData" class="text_center">
-      <h3 class="alert alert-warning text-center" v-text="'No More Data!'"></h3>
-    </div>
   </div>
 </template>
 
 <script>
+import { Toast } from 'mint-ui'
+
 export default {
   name: 'Loading',
   components: {},
@@ -29,15 +28,23 @@ export default {
     return {
     }
   },
-
   computed: {},
+  watch: {
+    moreData: function(newVal, oldVal) {
+      if (newVal === false) {
+        Toast({
+          message: 'No More Data!',
+          position: 'bottom'
+        })
+      }
+    }
+  },
   created() {
 
   },
   methods: {}
 }
 </script>
-
 <style lang="scss" scoped>
   #loadingBox{;padding: 1vh 1vw;
     .snake{display: inline-block}
